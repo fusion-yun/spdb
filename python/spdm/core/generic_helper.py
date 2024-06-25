2edb57fc0540aae9a48b9ac00b19cbc393ccac9a
Salmon <yuzhi@ipp.ac.cn>
2024-06-25 09:42:39 +0800

    [update]generic_helper

--- python/spdm/core/generic_helper.py
+++ python/spdm/core/generic_helper.py
@@ -1,7 +1,6 @@
 """ Generic Helper """
 
 import typing
-import types
 import inspect
 from copy import deepcopy, copy
 import functools
@@ -10,33 +9,26 @@ from spdm.utils.logger import logger
 _Ts = typing.TypeVarTuple("_Ts")
 
 
-def generic_specification(tp: type | typing.TypeVar, specification: dict, name=None) -> type:
+def generic_specification(tp: type | typing.TypeVar, tp_map: dict) -> type:
     """若类型参数完全特化（specification），则返回新的类，而不是 typing._GenericAlias。
     例如：typing.List[int] -> List[int]，而不是 typing._GenericAlias。
     List[int] 类型具有类属性 __args__，用于存储类型参数。
 
     """
-    new_tp = tp
+
     if isinstance(tp, typing.TypeVar):
-        new_tp = specification.get(tp, None)
+        new_tp = tp_map.get(tp, None)
     elif isinstance(tp, typing._GenericAlias):
-        args = tuple([generic_specification(a, specification) for a in tp.__args__ if a is not type(None)])
+        args = tuple([generic_specification(a, tp_map) for a in tp.__args__ if a is not type(None)])
         args = tuple([a for a in args if a is not None])
         if len(args) > 0:
             new_tp = tp.__getitem__(args)
         else:
             new_tp = None
-
-    # elif isinstance(tp, type):
-    #     new_tp = type(name, (tp,), generic_specification(tp.__dir__, specification))
-
-    elif isinstance(tp, dict):
-        new_tp = {k: generic_specification(v, specification, k) for k, v in tp.items()}
-        new_tp = {k: v for k, v in new_tp.items() if v is not None}
-        if len(new_tp) == 0:
-            new_tp = None
-    elif inspect.isfunction(tp):
-        annotations = generic_specification(getattr(tp, "__annotations__", {}), specification)
+    elif isinstance(tp, type):
+        new_tp = tp
+    elif inspect.isfunction(tp) or inspect.ismethod(tp):
+        annotations = generic_specification(getattr(tp, "__annotations__", {}), tp_map)
         if annotations is None:
             new_tp = None
         else:
@@ -47,67 +39,75 @@ def generic_specification(tp: type | typing.TypeVar, specification: dict, name=N
     return new_tp
 
 
+def spec_members(members: dict, cls, tp_map) -> dict:
+    if not issubclass(cls, typing.Generic) or cls is typing.Generic:
+        return members
+
+    if members is None:
+        members = {}
+
+    for k, m in inspect.getmembers(cls):
+        if k not in members and not k.startswith("__") and (tp_hint := generic_specification(m, tp_map)) is not None:
+            members[k] = tp_hint
+
+    ann = members.get("__annotations__", {})
+
+    ann.update({k: generic_specification(v, tp_map) for k, v in cls.__annotations__.items() if k not in ann})
+
+    members["__annotations__"] = ann
+
+    for idx, orig_base in enumerate(cls.__orig_bases__):
+        if not isinstance(orig_base, typing._GenericAlias):
+            continue
+
+        base = cls.__bases__[idx]
+
+        if not issubclass(base, typing.Generic) or base is typing.Generic:
+            continue
+        base_tp_map = {k: generic_specification(v, tp_map) for k, v in (zip(base.__parameters__, orig_base.__args__))}
+
+        members = spec_members(members, base, base_tp_map)
+
+    return members
+
+
 class GenericHelper(typing.Generic[*_Ts]):
     """A helper class for typing generic."""
 
     @typing._tp_cache
     def __class_getitem__(cls, item):
-
         alias = super().__class_getitem__(item)
 
-        if cls is GenericHelper:
+        if cls is GenericHelper or len(alias.__parameters__) > 0:
             return alias
 
+        orig_cls = alias.__origin__
+
         cls_name = f"{cls.__name__}[" + ",".join(typing._type_repr(a) for a in alias.__args__) + "]"
-
-        specification = dict(zip(cls.__parameters__, alias.__args__))
-
-        # specification = {k: v for k, v in specification.items() if k is not v}
-
-        # if len(specification) == 0:
-        #     return alias
-            # tp_spec = {k: generic_specification(getattr(cls, k, None), specification, k) for k in dir(cls)}
-
-            # tp_spec = {k: v for k, v in tp_spec.items() if v is not None}
-        members = generic_specification(
-            {
-                k: member
-                for k, member in inspect.getmembers(cls)
-                if inspect.isfunction(member)
-                or inspect.ismethod(member)
-                or isinstance(member, (typing._GenericAlias, typing.TypeVar))
-            },
-            specification,
-        )
-
-        if members is None:
-            members = {}
-
-        annotations = generic_specification(typing.get_type_hints(cls), specification)
-
-        if annotations is not None:
-            members["__annotations__"] = annotations
 
         n_cls = type(
             cls_name,
             (cls,),
             {
-                **members,
+                **spec_members(None, orig_cls, dict(zip(orig_cls.__parameters__, alias.__args__))),
                 "__module__": cls.__module__,
                 "__package__": getattr(cls, "__package__", None),
             },
         )
 
-        if len(alias.__parameters__) > 0:
-            alias.__origin__ = n_cls
-            return alias
-        else:
-            return n_cls
+        return n_cls
 
-    # return _TGenericAlias(
-    #     alias.__origin__,
-    #     alias.__args__,
-    #     inst=alias._inst,
-    #     name=alias._name,
-    #     _paramspec_tvars=alias._paramspec_tvars,
-    # )[()]
+
+__all__ = ["GenericHelper"]
+# if len(alias.__parameters__) > 0:
+#     # alias.__origin__ = n_cls
+# else:
+#     return n_cls
+
+# return _TGenericAlias(
+#     alias.__origin__,
+#     alias.__args__,
+#     inst=alias._inst,
+#     name=alias._name,
+#     _paramspec_tvars=alias._paramspec_tvars,
+# )[()]
--- tests/core/test_generic.py
+++ tests/core/test_generic.py
@@ -54,6 +54,55 @@ class TestGeneric(unittest.TestCase):
             },
         )
 
+    def test_inheritance(self):
+        _T0 = typing.TypeVar("_T0")
+        _T1 = typing.TypeVar("_T1")
+        _T2 = typing.TypeVar("_T2")
+        _S0 = typing.TypeVar("_S0")
+        _S1 = typing.TypeVar("_S1")
+        _S2 = typing.TypeVar("_S2")
+
+        class Foo(GenericHelper[_T0, _T1, _T2]):
+            TValue = _T2
+            foo: _T0
+            boo: _T1 = 10
+            koo: typing.Tuple[_T1, _T2]
+
+            def func(self, a: _T0, b: _T2) -> _T1:
+                pass
+
+        class Goo(GenericHelper[_T0, _T1, _T2]):
+            GType = _T2
+            goo: _T0
+            koo: typing.Tuple[_T1, _T2]
+
+            def func(self, a: _T0, b: _T2) -> _T1:
+                pass
+
+        class Boo(Foo[int, _S1, _T2], Goo[int, _T0, _T2]):
+            BooType1: _S1 = 5
+            BooType2: _T0
+
+        class Aoo(Boo[_T0, _T1, _T2]):
+            AooType: _T2
+
+        tp = Aoo[complex, str, float]
+
+        self.assertEqual(tp.boo, 10)
+
+        self.assertDictEqual(
+            typing.get_type_hints(tp),
+            {
+                "goo": int,
+                "koo": typing.Tuple[complex, str],
+                "foo": int,
+                "boo": complex,
+                "BooType1": complex,
+                "BooType2": float,
+                "AooType": float,
+            },
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
